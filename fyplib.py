a7e6932cdbd19c77b31562df6f2e3cb64da4ee04
Kasper Grønbek <47602813+kaspergroenbek98@users.noreply.github.com>
2020-02-19 21:39:13 +0100

    Changed to identify hashtag
    A function that identifies hashtags from input data

--- fyplib.py
+++
@@ -1,6 +0,0 @@
-def csv_reader:  # Read the data into a header and a data np.array
-	with open('data.csv', encoding='latin1') as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',')
-    	data = np.array([np.array(line) for line in csv_reader])
-	header, data = data[0,:], data[1:,:]
-	return header, data
---
+++ identify_hashtags.py
@@ -0,0 +1,12 @@
+def identify_hashtags(data)	
+	col = np.zeros((data.shape[0],1), 'str')
+	data = np.append(data, col, axis=1)
+	print(data.shape) # Note that shape starts at 1 and ends at 16, so the interval is [0:15]
+
+	for i, text in enumerate(data[:,6]):
+    		results = re.findall(r"#\w+", text) # Finds matches and returns them as an iterable
+    		if results:
+        		data[i,15] = ' '.join(results)
+    		else:
+        		data[i,15] = ''
+	hashtags = set(hashtag for row in data[:,15] for hashtag in row.split())
